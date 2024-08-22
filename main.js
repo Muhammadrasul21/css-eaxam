a08070a659354245298e3b2f214329eb3bc6095f
Muhammadrasul21 <theboy6878@gmail.com>
2024-08-22 23:39:29 +0500

    fixed navbar

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ function showHamburger() {
 function closeHamburger() {
     hamburger.style.display = "none"
 }
+
+
+const header = document.querySelector(".header")
+
+window.addEventListener("scroll", ()=>{
+if(window.scrollY > 0){
+    header.classList.add("shrink")
+}else{
+    header.classList.remove("shrink")
+}
+})
